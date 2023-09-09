1cc5e31d3c127bfaaa60160e5a15a810f4bf76c2
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2023-09-09 18:19:06 +0300

    Task : Define all controllers as template

---
+++ src/main/java/com/example/demo/controller/StatisticsController.java
@@ -0,0 +1,11 @@
+package com.example.demo.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/statistics")
+@RequiredArgsConstructor
+public class StatisticsController {
+}
