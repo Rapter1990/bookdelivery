6609f757683a13863e428f0f339070ed34d29463
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2023-09-09 23:46:24 +0300

    Task : Add JWTResponse and TokenRefreshResponse

---
+++ src/main/java/com/example/demo/payload/payload/JWTResponse.java
@@ -0,0 +1,22 @@
+package com.example.demo.payload.payload;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class JWTResponse {
+
+    private String token;
+    private String type = "Bearer";
+    private String refreshToken;
+    private Long id;
+    private String username;
+    private String email;
+    private String role;
+
+}
---
+++ src/main/java/com/example/demo/payload/payload/TokenRefreshResponse.java
@@ -0,0 +1,18 @@
+package com.example.demo.payload.payload;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class TokenRefreshResponse {
+
+    private String accessToken;
+    private String refreshToken;
+    private String tokenType = "Bearer";
+
+}
