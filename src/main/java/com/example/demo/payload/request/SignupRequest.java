00dec4213ca318fe4ceb8809571c96197423a9c6
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2023-09-10 21:31:16 +0300

    Task : Add Builder to LoginRequest, SignupRequest and TokenRefreshRequest

--- src/main/java/com/example/demo/payload/request/SignupRequest.java
+++ src/main/java/com/example/demo/payload/request/SignupRequest.java
@@ -7,12 +7,14 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class SignupRequest {
 
     @NotBlank
--- src/main/java/com/example/demo/payload/request/TokenRefreshRequest.java
+++ src/main/java/com/example/demo/payload/request/TokenRefreshRequest.java
@@ -2,12 +2,14 @@ package com.example.demo.payload.request;
 
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class TokenRefreshRequest {
 
     @NotBlank
