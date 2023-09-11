fc0dac3edaaf84c73e21fafa3a3fa55263a93966
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2023-09-11 20:54:06 +0300

    Revise RefreshTokenServiceImpl

--- src/main/java/com/example/demo/service/impl/RefreshTokenServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/RefreshTokenServiceImpl.java
@@ -8,6 +8,7 @@ import com.example.demo.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.Instant;
 import java.time.LocalDate;
