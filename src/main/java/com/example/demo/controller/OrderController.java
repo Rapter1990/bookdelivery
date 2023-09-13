6033959b981a2ac96d372e6ad4bec7b12e7f28d4
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2023-09-13 21:46:19 +0300

    Task: Write some comments

--- src/main/java/com/example/demo/controller/OrderController.java
+++ src/main/java/com/example/demo/controller/OrderController.java
@@ -10,4 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class OrderController {
 
     // Implement OrderService
+    // Can Create OrderSaveService for Create Order
+
 }
