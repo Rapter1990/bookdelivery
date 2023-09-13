6c312be439ee782dc503d4b0cd8c1a1b88127401
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2023-09-13 15:36:40 +0300

    Task : Implement givenOrderId_WhenOrderFound_ReturnOrderDTO, givenCustomerIdAndPaginationRequest_WhenOrdersFound_ReturnPageOrderDTOList and givenPaginatedFindAllRequest_WhenOrdersFound_ReturnPageOrderDTOList in OrderServiceImplTest

--- src/test/java/com/example/demo/service/impl/OrderServiceImplTest.java
+++ src/test/java/com/example/demo/service/impl/OrderServiceImplTest.java
@@ -9,16 +9,21 @@ import com.example.demo.model.Book;
 import com.example.demo.model.Order;
 import com.example.demo.model.OrderItem;
 import com.example.demo.model.User;
+import com.example.demo.payload.request.pagination.DateIntervalRequest;
+import com.example.demo.payload.request.pagination.PaginatedFindAllRequest;
+import com.example.demo.payload.request.pagination.PaginationRequest;
 import com.example.demo.repository.OrderRepository;
 import com.example.demo.service.OrderService;
 import com.example.demo.util.RandomUtil;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
 
 import java.math.BigDecimal;
-import java.util.Optional;
-import java.util.Set;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
@@ -95,7 +100,7 @@ class OrderServiceImplTest extends BaseServiceTest {
                 .build();
 
 
-        Order order = Order.builder()
+        Order mockOrder = Order.builder()
                 .id(orderId)
                 .user(mockUser)
                 .orderItems(Set.of(orderItem1,orderItem2))
@@ -120,7 +125,7 @@ class OrderServiceImplTest extends BaseServiceTest {
                 .build();
 
         // when
-        when(orderRepository.findById(orderId)).thenReturn(Optional.of(order));
+        when(orderRepository.findById(orderId)).thenReturn(Optional.of(mockOrder));
 
         // then
         OrderDTO actual = orderService.findOrderById(orderId);
@@ -135,12 +140,144 @@ class OrderServiceImplTest extends BaseServiceTest {
     }
 
     @Test
-    void findAllOrdersByCustomerId() {
+    void givenCustomerIdAndPaginationRequest_WhenOrdersFound_ReturnPageOrderDTOList() {
 
+        // Given
+        Long customerId = 1L;
+        Long orderId = 1L;
+        PaginationRequest paginationRequest = new PaginationRequest(0, 10);
+        PageRequest pageRequest = PageRequest.of(paginationRequest.getPage(), paginationRequest.getSize());
+
+        User mockUser = User.builder()
+                .email("mock@bookdelivery.com")
+                .fullName("User FullName")
+                .build();
+
+        String mockBookId1 = RandomUtil.generateUUID();
+        String mockBookId2 = RandomUtil.generateUUID();
+
+        Book mockBook1 = Book.builder()
+                .id(mockBookId1)
+                .name("Name")
+                .authorFullName("Author Full Name")
+                .isbn("1234567890")
+                .stock(321)
+                .price(BigDecimal.TEN)
+                .version(0L)
+                .build();
+
+        Book mockBook2 = Book.builder()
+                .id(mockBookId2)
+                .name("Name")
+                .authorFullName("Author Full Name")
+                .isbn("1234567890")
+                .stock(321)
+                .price(BigDecimal.TEN)
+                .version(0L)
+                .build();
+
+        OrderItem orderItem1 = OrderItem.builder()
+                .book(mockBook1)
+                .build();
+
+        OrderItem orderItem2 = OrderItem.builder()
+                .book(mockBook2)
+                .build();
+
+        Order mockOrder = Order.builder()
+                .id(orderId)
+                .user(mockUser)
+                .orderItems(Set.of(orderItem1,orderItem2))
+                .build();
+
+        // When
+        when(orderRepository.findAllByUser(eq(customerId), eq(pageRequest)))
+                .thenReturn(new PageImpl<>(Collections.singletonList(mockOrder)));
+
+        // Then
+        Page<OrderDTO> result = orderService.findAllOrdersByCustomerId(customerId, paginationRequest);
+        assertNotNull(result);
+        assertEquals(1, result.getTotalElements());
+
+        // Verify that the repository's findAllByUser method was called with the correct arguments
+        verify(orderRepository, times(1)).findAllByUser(eq(customerId), eq(pageRequest));
     }
 
     @Test
-    void findAllOrdersBetweenTwoDatesAndPagination() {
+    void givenPaginatedFindAllRequest_WhenOrdersFound_ReturnPageOrderDTOList() {
 
+        // Given
+        Calendar calendar1 = Calendar.getInstance();
+        calendar1.set(Calendar.YEAR, 2000);
+        calendar1.set(Calendar.MONTH, Calendar.SEPTEMBER);
+        calendar1.set(Calendar.DAY_OF_MONTH, 10);
+
+        Calendar calendar2 = Calendar.getInstance();
+        calendar1.set(Calendar.YEAR, 2000);
+        calendar1.set(Calendar.MONTH, Calendar.SEPTEMBER);
+        calendar1.set(Calendar.DAY_OF_MONTH, 13);
+
+        Date startDate = calendar1.getTime();
+        Date endDate = calendar2.getTime();
+        DateIntervalRequest dateIntervalRequest = new DateIntervalRequest(startDate, endDate);
+        PaginationRequest paginationRequest = new PaginationRequest(0, 10);
+        PageRequest pageRequest = PageRequest.of(paginationRequest.getPage(), paginationRequest.getSize());
+
+        Long orderId = 1L;
+
+        User mockUser = User.builder()
+                .email("mock@bookdelivery.com")
+                .fullName("User FullName")
+                .build();
+
+        String mockBookId1 = RandomUtil.generateUUID();
+        String mockBookId2 = RandomUtil.generateUUID();
+
+        Book mockBook1 = Book.builder()
+                .id(mockBookId1)
+                .name("Name")
+                .authorFullName("Author Full Name")
+                .isbn("1234567890")
+                .stock(321)
+                .price(BigDecimal.TEN)
+                .version(0L)
+                .build();
+
+        Book mockBook2 = Book.builder()
+                .id(mockBookId2)
+                .name("Name")
+                .authorFullName("Author Full Name")
+                .isbn("1234567890")
+                .stock(321)
+                .price(BigDecimal.TEN)
+                .version(0L)
+                .build();
+
+        OrderItem orderItem1 = OrderItem.builder()
+                .book(mockBook1)
+                .build();
+
+        OrderItem orderItem2 = OrderItem.builder()
+                .book(mockBook2)
+                .build();
+
+        Order mockOrder = Order.builder()
+                .id(orderId)
+                .user(mockUser)
+                .orderItems(Set.of(orderItem1,orderItem2))
+                .build();
+
+        // When
+        when(orderRepository.findAllByCreatedAtBetween(eq(dateIntervalRequest), eq(pageRequest)))
+                .thenReturn(new PageImpl<>(Collections.singletonList(mockOrder)));
+
+        // Then
+        Page<OrderDTO> result = orderService.findAllOrdersBetweenTwoDatesAndPagination(
+                new PaginatedFindAllRequest(dateIntervalRequest, paginationRequest));
+        assertNotNull(result);
+        assertEquals(1, result.getTotalElements());
+
+        // Verify that the repository's findAllByCreatedAtBetween method was called with the correct arguments
+        verify(orderRepository, times(1)).findAllByCreatedAtBetween(eq(dateIntervalRequest), eq(pageRequest));
     }
 }
