66febe61d6424682550ea175f93b7bb2c97a4371
AhmetAksunger <ahmetaksungerbilgi@gmail.com>
2023-09-13 04:02:04 +0300

    Task: Handle givenValidBookIdAndBookUpdateStockRequest_whenBookUpdated_thenReturnBook

--- src/main/java/com/example/demo/payload/request/book/BookUpdateStockRequest.java
+++ src/main/java/com/example/demo/payload/request/book/BookUpdateStockRequest.java
@@ -2,13 +2,11 @@ package com.example.demo.payload.request.book;
 
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class BookUpdateStockRequest {
--- src/test/java/com/example/demo/service/impl/BookServiceImplTest.java
+++ src/test/java/com/example/demo/service/impl/BookServiceImplTest.java
@@ -4,6 +4,7 @@ import com.example.demo.base.BaseServiceTest;
 import com.example.demo.exception.book.BookNotFoundException;
 import com.example.demo.model.Book;
 import com.example.demo.payload.request.book.BookCreateRequest;
+import com.example.demo.payload.request.book.BookUpdateStockRequest;
 import com.example.demo.repository.BookRepository;
 import com.example.demo.util.RandomUtil;
 import org.junit.jupiter.api.Assertions;
@@ -99,5 +100,36 @@ class BookServiceImplTest extends BaseServiceTest {
         );
 
         Mockito.verify(bookRepository, Mockito.times(1)).findById(Mockito.anyString());
+    }
+
+    @Test
+    void givenValidBookIdAndBookUpdateStockRequest_whenBookUpdated_thenReturnBook() {
+
+        // Given
+        String mockBookId = RandomUtil.generateUUID();
+
+        BookUpdateStockRequest mockRequest = BookUpdateStockRequest.builder()
+                .stock(123)
+                .build();
+
+        Book mockBook = Book.builder()
+                .id(mockBookId)
+                .name("Name")
+                .authorFullName("Author Full Name")
+                .isbn("1234567890")
+                .stock(321)
+                .price(BigDecimal.TEN)
+                .version(0L)
+                .build();
+
+        // When
+        Mockito.when(bookRepository.findById(Mockito.anyString())).thenReturn(Optional.of(mockBook));
+
+        // Then
+        bookService.updateBookStockById(mockBookId, mockRequest);
+
+        Assertions.assertEquals(mockRequest.getStock(), mockBook.getStock());
+        Mockito.verify(bookRepository, Mockito.times(1)).findById(Mockito.anyString());
+        Mockito.verify(bookRepository, Mockito.times(1)).save(Mockito.any(Book.class));
     }
 }
