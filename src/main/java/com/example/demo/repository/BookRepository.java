f8dde06ec91aeaa73a32c9849e6b3dde1ca662de
AhmetAksunger <ahmetaksungerbilgi@gmail.com>
2023-09-13 15:26:29 +0300

    Revise: Remove Optional findAllBooks

--- src/main/java/com/example/demo/repository/BookRepository.java
+++ src/main/java/com/example/demo/repository/BookRepository.java
@@ -2,17 +2,9 @@ package com.example.demo.repository;
 
 import com.example.demo.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface BookRepository extends JpaRepository<Book, String> {
-
-
-    @Query("SELECT b FROM Book b")
-    Optional<List<Book>> findAllBooks();
 
 }
