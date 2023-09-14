d8bf99265f3e01285abef338762449a441be24e1
AhmetAksunger <ahmetaksungerbilgi@gmail.com>
2023-09-14 17:26:33 +0300

    Revise: Change OrderItemDTO & OrderItemMapper

--- src/main/java/com/example/demo/dto/OrderItemDTO.java
+++ src/main/java/com/example/demo/dto/OrderItemDTO.java
@@ -1,12 +1,27 @@
 package com.example.demo.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+
+import java.math.BigDecimal;
 
 @Getter
 @Builder
 public class OrderItemDTO {
 
     private Long id;
-    private BookDTO book;
+    private OrderItemBook book;
+
+    @Getter
+    @Builder
+    @AllArgsConstructor
+    public static class OrderItemBook {
+        private String id;
+        private String isbn;
+        private String name;
+        private String authorFullName;
+        private BigDecimal price;
+    }
+
 }
--- src/main/java/com/example/demo/model/mapper/order/OrderItemMapper.java
+++ src/main/java/com/example/demo/model/mapper/order/OrderItemMapper.java
@@ -1,11 +1,11 @@
 package com.example.demo.model.mapper.order;
 
 import com.example.demo.dto.OrderItemDTO;
+import com.example.demo.model.Book;
 import com.example.demo.model.OrderItem;
-import com.example.demo.model.mapper.book.BookMapper;
 import lombok.experimental.UtilityClass;
 
-import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Set;
 
@@ -16,16 +16,26 @@ public class OrderItemMapper {
     public OrderItemDTO toDTO(OrderItem orderItem) {
         return OrderItemDTO.builder()
                 .id(orderItem.getId())
-                .book(BookMapper.toDTO(orderItem.getBook()))
+                .book(toOrderItemBook(orderItem.getBook()))
                 .build();
     }
 
     public Set<OrderItemDTO> toDTO(Set<OrderItem> orderItems) {
 
         List<OrderItemDTO> orderItemDtos = orderItems.stream()
-                .map(OrderItemMapper::toDTO).toList();
+                .map(OrderItemMapper::toDTO)
+                .toList();
 
-        return new HashSet<>(orderItemDtos);
+        return new LinkedHashSet<>(orderItemDtos);
     }
 
+    private OrderItemDTO.OrderItemBook toOrderItemBook(Book source) {
+        return OrderItemDTO.OrderItemBook.builder()
+                .id(source.getId())
+                .name(source.getName())
+                .authorFullName(source.getAuthorFullName())
+                .isbn(source.getIsbn())
+                .price(source.getPrice())
+                .build();
+    }
 }
