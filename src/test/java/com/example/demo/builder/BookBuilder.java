0d75bb6cf4578ccbd2c0d10e28ad92c1e3aa7238
AhmetAksunger <ahmetaksungerbilgi@gmail.com>
2023-09-14 16:22:24 +0300

    Task: Create BookBuilder

---
+++ src/test/java/com/example/demo/builder/BookBuilder.java
@@ -0,0 +1,58 @@
+package com.example.demo.builder;
+
+import com.example.demo.model.Book;
+import com.example.demo.util.RandomUtil;
+
+import java.math.BigDecimal;
+
+public class BookBuilder extends BaseBuilder<Book> {
+    public BookBuilder() {
+        super(Book.class);
+    }
+
+    public BookBuilder withValidFields() {
+        return this
+                .withId(RandomUtil.generateUUID())
+                .withIsbn(RandomUtil.generateRandomString())
+                .withName(RandomUtil.generateRandomString())
+                .withAuthorFullName(RandomUtil.generateRandomString())
+                .withStock(RandomUtil.generateRandomInteger(1,100))
+                .withPrice(RandomUtil.generateRandomBigDecimal(1.0,200.0))
+                .withVersion(0L);
+    }
+
+    public BookBuilder withId(String id) {
+        data.setId(id);
+        return this;
+    }
+
+    public BookBuilder withIsbn(String isbn) {
+        data.setIsbn(isbn);
+        return this;
+    }
+
+    public BookBuilder withName(String name) {
+        data.setName(name);
+        return this;
+    }
+
+    public BookBuilder withAuthorFullName(String authorFullName) {
+        data.setAuthorFullName(authorFullName);
+        return this;
+    }
+
+    public BookBuilder withStock(Integer stock) {
+        data.setStock(stock);
+        return this;
+    }
+
+    public BookBuilder withPrice(BigDecimal price) {
+        data.setPrice(price);
+        return this;
+    }
+
+    public BookBuilder withVersion(Long version) {
+        data.setVersion(version);
+        return this;
+    }
+}
