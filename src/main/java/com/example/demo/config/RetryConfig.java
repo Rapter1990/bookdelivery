5bc5874c13ff40b94d65bd7819ca515dd7b3f65b
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2023-09-14 22:25:31 +0300

    Task : Define RetryConfig for Optimistic Locking process with its dependency

---
+++ src/main/java/com/example/demo/config/RetryConfig.java
@@ -0,0 +1,10 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.retry.annotation.EnableRetry;
+
+@Configuration
+@EnableRetry
+public class RetryConfig {
+
+}
