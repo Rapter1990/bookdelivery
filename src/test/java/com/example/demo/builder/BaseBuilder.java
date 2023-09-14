6012ed331ae0354f6581755e018708ff78472ec8
AhmetAksunger <ahmetaksungerbilgi@gmail.com>
2023-09-14 16:00:36 +0300

    Revise: Add JavaDocs to BaseBuilder

--- src/test/java/com/example/demo/builder/BaseBuilder.java
+++ src/test/java/com/example/demo/builder/BaseBuilder.java
@@ -2,17 +2,31 @@ package com.example.demo.builder;
 
 import lombok.SneakyThrows;
 
+/**
+ * Abstract base class for building objects of type T.
+ *
+ * @param <T> the type of object to be built
+ */
 public abstract class BaseBuilder<T> {
 
+    /**
+     * Constructs a new instance of the specified class and initializes it for building.
+     *
+     * @param clazz the class of type T
+     */
     @SneakyThrows
-    public BaseBuilder(Class<T> clazz){
-        this.clazz = clazz;
+    public BaseBuilder(Class<T> clazz) {
         this.data = clazz.getDeclaredConstructor().newInstance();
     }
 
     T data;
-    Class<T> clazz;
 
+
+    /**
+     * Builds and returns the constructed object of type T.
+     *
+     * @return the constructed object
+     */
     public T build() {
         return data;
     }
