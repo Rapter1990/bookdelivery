a8cc13e59518555082e718f9b92272522c6f72a0
AhmetAksunger <ahmetaksungerbilgi@gmail.com>
2023-09-14 04:41:10 +0300

    Revise: Add Service annotation to OrderSaveServiceImpl

--- src/main/java/com/example/demo/service/OrderSaveService.java
+++ src/main/java/com/example/demo/service/OrderSaveService.java
@@ -2,9 +2,7 @@ package com.example.demo.service;
 
 import com.example.demo.dto.OrderDTO;
 import com.example.demo.payload.request.order.CreateOrderRequest;
-import org.springframework.stereotype.Service;
 
-@Service
 public interface OrderSaveService {
 
     OrderDTO createOrder(CreateOrderRequest createOrderRequest);
--- src/main/java/com/example/demo/service/impl/OrderSaveServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/OrderSaveServiceImpl.java
@@ -3,7 +3,11 @@ package com.example.demo.service.impl;
 import com.example.demo.dto.OrderDTO;
 import com.example.demo.payload.request.order.CreateOrderRequest;
 import com.example.demo.service.OrderSaveService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
+@Service
+@RequiredArgsConstructor
 public class OrderSaveServiceImpl implements OrderSaveService {
 
     @Override
