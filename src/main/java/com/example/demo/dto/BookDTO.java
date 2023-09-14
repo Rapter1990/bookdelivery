502269475ec70b38ef4bd989cf9bb3337af4bf09
AhmetAksunger <ahmetaksungerbilgi@gmail.com>
2023-09-14 05:42:45 +0300

    Task: Add EqualsAndHashcode to BookDTO

--- src/main/java/com/example/demo/dto/BookDTO.java
+++ src/main/java/com/example/demo/dto/BookDTO.java
@@ -1,12 +1,14 @@
 package com.example.demo.dto;
 
 import lombok.Builder;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 
 import java.math.BigDecimal;
 
 @Getter
 @Builder
+@EqualsAndHashCode
 public class BookDTO {
 
     private String id;
