18f8a7248352f602a7bbddabb845fa5edcb39f93
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2023-09-15 10:23:55 +0300

    Revise and Task : Revise methods in StatisticsService and Define methods in OrderRepository

--- src/main/java/com/example/demo/repository/OrderRepository.java
+++ src/main/java/com/example/demo/repository/OrderRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 
 import java.time.LocalDateTime;
 
@@ -13,4 +14,11 @@ public interface OrderRepository extends JpaRepository<Order, Long>, JpaSpecific
     Page<Order> findAllByUserId(Long userId, Pageable orderPageable);
 
     Page<Order> findAllByCreatedAtBetween(LocalDateTime startDate, LocalDateTime endTime, Pageable orderPageable);
+
+    @Query("SELECT COUNT(DISTINCT o.id) FROM Order o WHERE o.user.id = :userId")
+    Integer countDistinctOrdersByUserId(Long userId);
+
+    @Query("SELECT COUNT(DISTINCT o.id) FROM Order o")
+    Integer countDistinctOrders();
+
 }
--- src/main/java/com/example/demo/service/StatisticsService.java
+++ src/main/java/com/example/demo/service/StatisticsService.java
@@ -1,12 +1,13 @@
 package com.example.demo.service;
 
 import com.example.demo.dto.OrderReportDTO;
+import com.example.demo.payload.request.pagination.PaginationRequest;
 import org.springframework.data.domain.Page;
 
 
 public interface StatisticsService {
 
-    Page<OrderReportDTO> getOrderStatisticsByCustomer(Long customerId);
-    Page<OrderReportDTO> getOrderStatistics();
+    OrderReportDTO getOrderStatisticsByCustomer(Long customerId, PaginationRequest paginationRequest);
+    OrderReportDTO getOrderStatistics(PaginationRequest paginationRequest);
 
 }
