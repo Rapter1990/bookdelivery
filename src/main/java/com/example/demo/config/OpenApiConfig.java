421ebd5092f6508a1ceb5015d71c3faaac129fd9
AhmetAksunger <ahmetaksungerbilgi@gmail.com>
2023-09-15 06:57:30 +0300

    Task: Create OpenApiConfig

---
+++ src/main/java/com/example/demo/config/OpenApiConfig.java
@@ -0,0 +1,31 @@
+package com.example.demo.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+
+@OpenAPIDefinition(
+        info = @Info(
+                contact = @Contact(
+                        name = "Sercan Noyan Germiyanoğlu | Ahmet Aksünger | Muhammet Oğuzhan Aydoğan",
+                        url = "https://github.com/Rapter1990/bookdelivery/"
+                ),
+                description = "Case Study - Sample Book Delivery App " +
+                        "(Spring Boot, Spring Security , Mysql, JUnit, Integration Test, Docker, Test Container, AOP) ",
+                title = "Book Delivery App API",
+                version = "1.0.0"
+        )
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        description = "JWT Token",
+        scheme = "bearer",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        in = SecuritySchemeIn.HEADER
+)
+public class OpenApiConfig {
+}
