5931951de311a832902128ee0f17450b5e24c241
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2023-09-15 10:25:00 +0300

    Task : Implement getOrderStatisticsByCustomer and getOrderStatistics in StatisticsController

--- src/main/java/com/example/demo/controller/StatisticsController.java
+++ src/main/java/com/example/demo/controller/StatisticsController.java
@@ -1,9 +1,15 @@
 package com.example.demo.controller;
 
+import com.example.demo.dto.OrderReportDTO;
+import com.example.demo.model.mapper.order.OrderReportMapper;
+import com.example.demo.payload.request.pagination.PaginationRequest;
+import com.example.demo.payload.response.CustomResponse;
+import com.example.demo.payload.response.order.OrderReportResponse;
+import com.example.demo.service.StatisticsService;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/statistics")
@@ -11,6 +17,28 @@ import org.springframework.web.bind.annotation.RestController;
 @SecurityRequirement(name = "bearerAuth")
 public class StatisticsController {
 
-    // Implement StatisticsService
+    private final StatisticsService statisticsService;
+
+    @GetMapping("/{customerId}")
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN','ROLE_CUSTOMER')")
+    public CustomResponse<OrderReportResponse> getOrderStatisticsByCustomer(
+            @PathVariable Long customerId,
+            @RequestBody PaginationRequest paginationRequest
+    ) {
+
+        OrderReportDTO orderReportDtosByCustomer = statisticsService.getOrderStatisticsByCustomer(customerId,paginationRequest);
+        OrderReportResponse orderReportResponse = OrderReportMapper.toOrderReportResponse(orderReportDtosByCustomer);
+        return CustomResponse.ok(orderReportResponse);
+
+    }
+
+    @GetMapping("/allstatistics")
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN','ROLE_CUSTOMER')")
+    public CustomResponse<OrderReportResponse> getOrderStatistics(@RequestBody PaginationRequest paginationRequest) {
+
+        OrderReportDTO orderReportAllDtos = statisticsService.getOrderStatistics(paginationRequest);
+        OrderReportResponse orderReportResponse = OrderReportMapper.toOrderReportResponse(orderReportAllDtos);
+        return CustomResponse.ok(orderReportResponse);
+    }
 
 }
