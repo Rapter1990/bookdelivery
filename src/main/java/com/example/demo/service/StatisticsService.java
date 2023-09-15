5bafeefbb7d4c0f9f18a0c685d0d901a4fa277cc
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2023-09-15 09:34:39 +0300

    Task : Create getOrderStatisticsByCustomer and getOrderStatistics in StatisticsService

--- src/main/java/com/example/demo/service/StatisticsService.java
+++ src/main/java/com/example/demo/service/StatisticsService.java
@@ -1,8 +1,12 @@
 package com.example.demo.service;
 
+import com.example.demo.dto.OrderReportDTO;
+import org.springframework.data.domain.Page;
+
+
 public interface StatisticsService {
 
-    // Order Statistics For Customer -> Java Stream
+    Page<OrderReportDTO> getOrderStatisticsByCustomer(Long customerId);
+    Page<OrderReportDTO> getOrderStatistics();
 
-    // All Order Statistics -> Java Stream
 }
