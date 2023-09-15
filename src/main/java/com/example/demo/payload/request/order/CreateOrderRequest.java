29a4919c7cb7d9046795a18bceca52c1a64d260c
AhmetAksunger <ahmetaksungerbilgi@gmail.com>
2023-09-15 03:59:27 +0300

    Task: Add Builder to CreateOrderRequest & OrderItemRequest

--- src/main/java/com/example/demo/payload/request/order/CreateOrderRequest.java
+++ src/main/java/com/example/demo/payload/request/order/CreateOrderRequest.java
@@ -2,6 +2,7 @@ package com.example.demo.payload.request.order;
 
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -10,6 +11,7 @@ import java.util.Set;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class CreateOrderRequest {
 
     @Valid
--- src/main/java/com/example/demo/payload/request/order/OrderItemRequest.java
+++ src/main/java/com/example/demo/payload/request/order/OrderItemRequest.java
@@ -3,12 +3,14 @@ package com.example.demo.payload.request.order;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class OrderItemRequest {
 
     @NotBlank
