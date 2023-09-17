5833145abbc6d93ebe462315abb9b06076f40f7a
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2023-09-17 16:44:01 +0300

    Add : Add Java Doc for Identity class

--- src/main/java/com/example/demo/util/Identity.java
+++ src/main/java/com/example/demo/util/Identity.java
@@ -10,11 +10,20 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
+/**
+ * A component representing the identity of the currently authenticated user.
+ * This class provides a method to retrieve the custom user details associated with the authenticated user.
+ */
 @Component
 @Scope(value = BeanScope.SCOPE_REQUEST, proxyMode = ScopedProxyMode.TARGET_CLASS)
 @RequiredArgsConstructor
 public class Identity {
 
+    /**
+     * Retrieve the custom user details of the currently authenticated user.
+     *
+     * @return The CustomUserDetails object containing user details.
+     */
     public CustomUserDetails getCustomUserDetails() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         UserDetails userDetails = (UserDetails) authentication.getPrincipal();
