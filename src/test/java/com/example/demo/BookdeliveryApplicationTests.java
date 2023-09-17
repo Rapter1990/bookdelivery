fbea87a9207c477010fc8f449a7fe94070f0f77d
Leonardo Silveira <sombriks@gmail.com>
2023-09-17 14:57:06 -0300

    fix for failing test

--- src/test/java/com/example/demo/BookdeliveryApplicationTests.java
+++ src/test/java/com/example/demo/BookdeliveryApplicationTests.java
@@ -1,12 +1,13 @@
 package com.example.demo;
 
+import com.example.demo.base.AbstractTestContainerConfiguration;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
-class BookdeliveryApplicationTests {
+class BookdeliveryApplicationTests extends AbstractTestContainerConfiguration {
 
 	@Test
 	void contextLoads() {
